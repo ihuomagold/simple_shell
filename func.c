70ab8a58745ce32e8af0876860c862ef0d6e1045
Ihuoma Ogbuji <ihuomagold53@gmail.com>
2022-09-02 04:30:26 -0700

    more functions

--- func.c
+++ func.c
@@ -32,3 +32,32 @@ void _puts(char *str)
 	_putchar('\n');
 	return;
 }
+
+/**
+ *  _atoi - convert to an integer
+ *  @s: string
+ *  Return: int
+ */
+
+int _atoi(char *s)
+{
+	int i, j, n, x;
+
+	i = n = 0;
+	x = 1;
+
+	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+			x *= -1;
+		i++;
+	}
+
+	j = i;
+	while ((s[j] >= '0') && (s[j] <= '9'))
+	{
+		n = (n * 10) + x * ((s[j]) - '0');
+		j++;
+	}
+	return (n);
+}

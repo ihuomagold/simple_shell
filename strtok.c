941e0680f070d5504b6362f9cec6d456a54ca8d6
Yirga Mulaw <infoyirga12@gmail.com>
2022-09-07 10:33:55 +0000

    strtok

--- strtok.c
+++ strtok.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-<<<<<<< HEAD
 /**
  * check_delim - Checks If A Character Match Any Char *
  * @c: Character To Check
@@ -7,18 +6,6 @@
  * Return: 1 Succes, 0 Failed
  */
 unsigned int check_delim(char c, const char *str)
-=======
-
-/**
- * delim_check - Checks If A Character Matches Any Char *
- * @c: Character to be checked
- * @str: String To Check
- *
- * Return: 1 Success, 0 Fail
- */
-
-unsigned int delim_check(char c, const char *str)
->>>>>>> 48f82fa4cd97283ad62d5f486c45b96f31ab8956
 {
 	unsigned int i;
 
@@ -31,21 +18,12 @@ unsigned int delim_check(char c, const char *str)
 }
 
 /**
-<<<<<<< HEAD
  * _strtok - Token A String Into Token (strtrok)
  * @str: String
  * @delim: Delimiter
  * Return: Pointer To The Next Token Or NULL
  */
-=======
- * _strtok - Token A String Into Token (strtok)
- * @str: String
- * @delim: Delimiter
- *
- * Return: Pointer To The Next Token Or NULL
- */
 
->>>>>>> 48f82fa4cd97283ad62d5f486c45b96f31ab8956
 char *_strtok(char *str, const char *delim)
 {
 	static char *ts;
@@ -59,11 +37,7 @@ char *_strtok(char *str, const char *delim)
 		return (NULL);
 	for (i = 0; ts[i] != '\0'; i++)
 	{
-<<<<<<< HEAD
 		if (check_delim(ts[i], delim) == 0)
-=======
-		if (delim_check(ts[i], delim) == 0)
->>>>>>> 48f82fa4cd97283ad62d5f486c45b96f31ab8956
 			break;
 	}
 	if (nt[i] == '\0' || nt[i] == '#')
@@ -75,11 +49,7 @@ char *_strtok(char *str, const char *delim)
 	nt = ts;
 	for (i = 0; nt[i] != '\0'; i++)
 	{
-<<<<<<< HEAD
 		if (check_delim(nt[i], delim) == 1)
-=======
-		if (delim_check(nt[i], delim) == 1)
->>>>>>> 48f82fa4cd97283ad62d5f486c45b96f31ab8956
 			break;
 	}
 	if (nt[i] == '\0')

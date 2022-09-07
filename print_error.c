26717af33096a5116a2ba8ee620786c2fb439843
Ihuoma Ogbuji <ihuomagold53@gmail.com>
2022-09-07 01:56:54 -0700

    handle output error

---
+++ print_error.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+
+/**
+ * _prerror - Print custom error
+ * @argv:Program Name
+ * @c:Error Count
+ * @cmd: Command
+ *
+ * Return: Void
+ *
+ */
+
+void _prerror(char **argv, int c, char **cmd)
+{
+	char *er = _itoa(c);
+
+	_PRINTF(argv[0]);
+	_PRINTF(": ");
+	_PRINTF(er);
+	_PRINTF(": ");
+	_PRINTF(cmd[0]);
+	_PRINTF(": Illegal number: ");
+	_PRINTF(cmd[1]);
+	_PRINTF("\n");
+	free(er);
+}
